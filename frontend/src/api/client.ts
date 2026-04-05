ab3ca5233627fe3046d98f1087dbb47741180d3e
Prakash9019 <plsprakash2003@gmail.com>
2026-04-05 17:19:23 +0530

    added

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 import { store } from '../store/store';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || "https://multi-tenant-tey5.vercel.app/api/v1",
+  // 'http://localhost:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
